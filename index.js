5c758e9e603f8778950f2d86b649cd02fed19e10
anirbanmajumder0 <81350357+anirbanmajumder0@users.noreply.github.com>
2023-02-11 01:29:02 +0530

    aaaa

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const cheerio = require('cheerio')
+const axios = require('axios')
+const path = require('path')
+const app = express()
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
+
+
+const getPostTitles = async () => {
+	try {
+		const { data } = await axios.get(
+			'https://isthereanydeal.com/specials/'
+		);
+		const $ = cheerio.load(data);
+		var postTitles = [];
+		const exp = [];
+
+		$('div.bundle-container-outer.giveaway > div > div > div > div.bundle-title > a').each((_idx, el) => {
+			const postTitle = $(el).text()
+			const postUrl = $(el).attr('href')
+			postTitles.push([postTitle, postUrl])
+		});
+		$('div.bundle-container-outer.giveaway > div > div > div > div.bundle-time').each((_idx, el) => {
+			const date = $(el).text()
+			exp.push(date)
+		});
+
+		postTitles = postTitles.map((e,i) => [e[0],e[1],exp[i]])
+		postTitles = postTitles.filter((e) => !(e[2].includes('expired')) && (e[0].includes('Steam') || e[0].includes('Epic Games')))
+
+		return postTitles
+	} catch (error) {
+		throw error;
+	}
+};
+
+getPostTitles()
+    .then((postTitles) => games = postTitles);
+
+// Handling GET / request
+app.get('/', (req, res) => {
+	console.log(games)
+	res.render('index', {title:"Free Games Today" ,lists: games })
+})
+//app.get('/', (req, res) => {
+//	res.sendFile(path.join(__dirname, 'index.html'))
+//})
+app.listen(3000, () => {
+	console.log("Server is Running")
+})
